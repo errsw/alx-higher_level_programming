fff9254cd5f5ecb4d131f381a906282618075bcf
errsw <salwa.cpt@gmail.com>
2024-04-02 20:01:33 -0700

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,3 @@
-vi 2-square
 #!/usr/bin/python3
 """Square."""
 
@@ -13,6 +12,6 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

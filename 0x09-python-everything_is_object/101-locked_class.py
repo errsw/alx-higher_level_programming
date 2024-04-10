c2650c8d66054ede6c8a380844f78f38883f3f18
errsw <salwa.cpt@gmail.com>
2024-04-10 15:24:06 -0700

    tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""define a locked class"""
 class LockedClass:
-    __slots__ = ['first_name']
+    """ 
+    Lockedclass for attribute first name
+    """
+    __slots__ = ["first_name"]
